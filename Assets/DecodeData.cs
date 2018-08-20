eafde65a621520c1d624374720e746c2dbed06e9
renfengyi <447336761@qq.com>
2018-08-20 16:04:40 +0800

    commit all file

---
+++ Assets/DecodeData.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using UnityEngine;
+using Nethereum.Contracts;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.JsonRpc.UnityClient;
+using Nethereum.Contracts.CQS;
+
+public class DecodeData : MonoBehaviour {
+
+    [FunctionOutput]
+    public class GetDataDTO
+    {
+        [Parameter("uint64", "birthTime", 1)]
+        public ulong BirthTime { get; set; }
+
+        [Parameter("string", "userName", 2)]
+        public string UserName { get; set; }
+
+        [Parameter("uint16", "starterId", 3)]
+        public int StarterId { get; set; }
+
+        [Parameter("uint16", "currLocation", 4)]
+        public int CurrLocation { get; set; }
+
+        [Parameter("bool", "isBusy", 5)]
+        public bool IsBusy { get; set; }
+
+        [Parameter("address", "owner", 6)]
+        public string Owner { get; set; }
+
+    }
+
+    [FunctionOutput]
+    public class GetR
+    {
+        [Parameter("int256", "r", 1)]
+        public ulong r { get; set; }
+
+    }
+
+    void Start ()
+	{
+        StartCoroutine(GetMultiply());
+        //StartCoroutine(GetData());
+        //StartCoroutine(GetArrayUInt256());
+    }
+
+    public IEnumerator GetMultiply() {
+        var contractAddress = "0xcb2a82f0ecfcaefe43c6ccb47ed66f4d8162c46b";
+        var url = "http://localhost:8545";
+        var getDataCallUnityRequest = new EthCallUnityRequest(url);
+        /*
+        [{'anonymous': false,'inputs': [{'indexed': true,'name': 'a','type': 'int256'},{'indexed': true,'name': 'sender','type': 'address'},{'indexed': false,'name': 'result','type': 'int256'}],'name': 'Multiplied','type': 'event'},{'constant': false,'inputs': [{'name': 'a','type': 'int256'}],'name': 'multiply','outputs': [{'name': 'r','type': 'int256'}],'payable': false,'stateMutability': 'nonpayable','type': 'function'},{'inputs': [{'name': 'multiplier','type': 'int256'}],'payable': false,'stateMutability':'nonpayable','type': 'constructor'}]
+        */
+        var contract = new Contract(null, @"[{'anonymous': false,'inputs': [{'indexed': true,'name': 'a','type': 'int256'},{'indexed': true,'name': 'sender','type': 'address'},{'indexed': false,'name': 'result','type': 'int256'}],'name': 'Multiplied','type': 'event'},{'constant': false,'inputs': [{'name': 'a','type': 'int256'}],'name': 'multiply','outputs': [{'name': 'r','type': 'int256'}],'payable': false,'stateMutability': 'nonpayable','type': 'function'},{'inputs': [{'name': 'multiplier','type': 'int256'}],'payable': false,'stateMutability':'nonpayable','type': 'constructor'}]", contractAddress);
+        var function = contract.GetFunction("multiply");
+        var callInput = function.CreateCallInput(12);
+
+
+        yield return getDataCallUnityRequest.SendRequest(callInput, Nethereum.RPC.Eth.DTOs.BlockParameter.CreateLatest());
+        var result = getDataCallUnityRequest.Result;
+        var thing = new GetR();
+        var output = function.DecodeDTOTypeOutput<GetR>(thing, result);
+        Debug.Log("r " + output.r);
+    }
+
+    public IEnumerator GetData()
+    {
+        var contractAddress = "0x786a30e1ab0c58303c85419b9077657ad4fdb0ea";
+        var url = "http://localhost:8545";
+        var getDataCallUnityRequest = new EthCallUnityRequest(url);
+        var contract = new Contract(null, @"[{ 'constant':false,'inputs':[],'name':'getData','outputs':[{'name':'birthTime','type':'uint64'},{'name':'userName','type':'string'},{'name':'starterId','type':'uint16'},{'name':'currLocation','type':'uint16'},{'name':'isBusy','type':'bool'},{'name':'owner','type':'address'}],'payable':false,'stateMutability':'nonpayable','type':'function'}]", contractAddress);
+        var function = contract.GetFunction("getData");
+        var callInput = function.CreateCallInput();
+
+        yield return getDataCallUnityRequest.SendRequest(callInput, Nethereum.RPC.Eth.DTOs.BlockParameter.CreateLatest());
+        var result = getDataCallUnityRequest.Result;
+        var thing = new GetDataDTO();
+        var output = function.DecodeDTOTypeOutput<GetDataDTO>(thing, result);
+        Debug.Log("birth block " + output.BirthTime);
+        Debug.Log("curr location " + output.CurrLocation);
+        Debug.Log("busy" + output.IsBusy);
+        Debug.Log("starterid " + output.StarterId);
+        Debug.Log("userName " + output.UserName);
+        Debug.Log("ownerAddress " + output.Owner);
+    }
+
+    public class ArrayUint256DynamicDeployment : ContractDeploymentMessage
+    {
+        public static string BYTECODE = "608060405234801561001057600080fd5b50610127806100206000396000f300608060405260043610603e5763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416634b04bc0481146043575b600080fd5b348015604e57600080fd5b50605560a3565b60408051602080825283518183015283519192839290830191858101910280838360005b83811015608f5781810151838201526020016079565b505050509050019250505060405180910390f35b6040805160028082526060808301845292602083019080388339019050509050600181600081518110151560d357fe5b6020908102909101015280516002908290600190811060ee57fe5b60209081029091010152905600a165627a7a72305820aa3f79a3ff9e580c10090fb0e8830490f2acbd918c76c8488c40bdaf1c9180800029";
+        public ArrayUint256DynamicDeployment() : base(BYTECODE) { }
+        public ArrayUint256DynamicDeployment(string byteCode) : base(byteCode) { }
+    }
+
+    [Function("GiveMeTheArray", "uint256[]")]
+	public class GiveMeTheArrayFunction:FunctionMessage
+	{
+
+	}
+
+	[FunctionOutput]
+	public class GiveMeTheArrayOutputDTO:IFunctionOutputDTO
+	{
+		[Parameter("uint256[]", "result", 1)]
+		public List<BigInteger> Result {get; set;}
+	}
+
+
+//Sample of new features / requests
+public IEnumerator GetArrayUInt256()
+    { 
+		var url = "http://localhost:8545";
+        var privateKey = "0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7";
+        var account = "0x12890d2cce102216644c59daE5baed380d84830c";
+        //initialising the transaction request sender
+        var transactionRequest = new TransactionSignedUnityRequest(url, privateKey, account);
+        //deploy the contract and True indicates we want to estimate the gas
+        yield return transactionRequest.SignAndSendDeploymentContractTransaction<ArrayUint256DynamicDeployment>();
+
+        if (transactionRequest.Exception != null)
+        {
+           Debug.Log(transactionRequest.Exception.Message);
+           yield break;
+        }
+
+        var transactionHash = transactionRequest.Result;
+
+        //create a poll to get the receipt when mined
+        var transactionReceiptPolling = new TransactionReceiptPollingRequest(url);
+        //checking every 2 seconds for the receipt
+        yield return transactionReceiptPolling.PollForReceipt(transactionHash, 2);
+        var deploymentReceipt = transactionReceiptPolling.Result;
+
+        //Query request using our acccount and the contracts address (no parameters needed and default values)
+        var queryRequest = new QueryUnityRequest<GiveMeTheArrayFunction,GiveMeTheArrayOutputDTO>(url, account);
+        yield return queryRequest.Query(deploymentReceipt.ContractAddress);
+
+        //Getting the dto response already decoded
+		var dtoResult = queryRequest.Result;
+
+		Debug.Log (dtoResult.Result [0]);
+		Debug.Log (dtoResult.Result [1]);
+	}
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/EtherTransfer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.ABI.Model;
+using Nethereum.Contracts;
+using Nethereum.Contracts.CQS;
+using Nethereum.Contracts.Extensions;
+using Nethereum.JsonRpc.UnityClient;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Util;
+using UnityEngine;
+
+public class EtherTransfer : MonoBehaviour {
+
+    
+    // Use this for initialization
+    void Start () {
+
+        StartCoroutine(TransferEther());
+    }
+
+
+    //Sample of new features / requests
+    public IEnumerator TransferEther()
+    {
+        var url = "http://localhost:8545";
+        var privateKey = "0xa5ca770c997e53e182c5015bcf1b58ba5cefe358bf217800d8ec7d64ca919edd";
+        var account = "0x47E95DCdb798Bc315198138bC930758E6f399f81";
+        //initialising the transaction request sender
+        var ethTransfer = new EthTransferUnityRequest(url, privateKey, account);
+        
+        var receivingAddress = "0x09f8F8c219D94A5db8Ee466dC072748603A7A0D9";
+        yield return ethTransfer.TransferEther("0x09f8F8c219D94A5db8Ee466dC072748603A7A0D9", 1.1m, 2);
+
+        if (ethTransfer.Exception != null)
+        {
+            Debug.Log(ethTransfer.Exception.Message);
+            yield break;
+        }
+
+        var transactionHash = ethTransfer.Result;
+
+        Debug.Log("Transfer transaction hash:" + transactionHash);
+
+        //create a poll to get the receipt when mined
+        var transactionReceiptPolling = new TransactionReceiptPollingRequest(url);
+        //checking every 2 seconds for the receipt
+        yield return transactionReceiptPolling.PollForReceipt(transactionHash, 2);
+        
+        Debug.Log("Transaction mined");
+
+        var balanceRequest = new EthGetBalanceUnityRequest(url);
+        yield return balanceRequest.SendRequest(receivingAddress, BlockParameter.CreateLatest());
+        
+        
+        Debug.Log("Balance of account:" + UnitConversion.Convert.FromWei(balanceRequest.Result.Value));
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
